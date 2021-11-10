dbb559bae40b4c50a2285010381223ff1b233e6d
Eric-Pessoa <pessoa.eric@gmail.com>
2021-11-09 23:23:52 -0300

    chore: pagina ong

--- Global-Impact/Global-Impact/Controllers/OngController.cs
+++ Global-Impact/Global-Impact/Controllers/OngController.cs
@@ -1,14 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Global_Impact.Persistence;
+using Global_Impact.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Global_Impact.Controllers
 {
     public class OngController : Controller
     {
+        private WefeedContext _context;
+
+        public OngController(WefeedContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
+        {
+            var listarOngs = _context.ONGs.Include(h => h.Endereco).ToList();
+            return View(listarOngs);
+        }
+
+        public IActionResult PaginaOng()
         {
             return View();
         }
