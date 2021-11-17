7a061557d01990c5cc17770bdda5d89b15d1441b
giovannadml <gih_mello@outlook.com>
2021-11-17 18:35:03 -0300

    api de endereço funcionando

---
+++ Global-Impact/Global-Impact/wwwroot/js/buscarCEP.js
@@ -0,0 +1,12 @@
+﻿function buscarCEP() {
+    var cep = document.querySelector("#cep").value;
+    var buscar = document.querySelector("#btnBuscar");
+    if (cep.length == 9) {
+        var inputCep = document.querySelector("#cepForm");
+        var inputUrl = document.querySelector("#urlForm");
+        var url = window.location.href
+        inputCep.value = cep.replace(/-/g, "")
+        inputUrl.value = url;
+        buscar.click();
+    }
+}
